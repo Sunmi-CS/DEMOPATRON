40206ea68d62b0ea2570f255f3faf1cf0d740b94
Sunmi-CS <sunmicasano@gmail.com>
2025-10-17 15:30:12 -0500

    SE EDITO

--- app/src/main/java/com/tecsup/demopatron/presentacion/MainActivity.kt
+++ app/src/main/java/com/tecsup/demopatron/presentacion/MainActivity.kt
@@ -4,54 +4,54 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.tecsup.demopatron.ui.theme.DemoPATRONTheme
-
 import com.tecsup.demopatron.presentacion.screens.InstructorFormScreen
 import com.tecsup.demopatron.presentacion.screens.InstructorListScreen
+import com.tecsup.demopatron.ui.theme.DemoPATRONTheme
 import com.tecsup.demopatron.viewmodel.InstructorViewModel
 
-
 class MainActivity : ComponentActivity() {
-    private val MainActivity.viewModel: InstructorViewModel by viewModel
 
+    private val viewModel: InstructorViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MaterialTheme {
+            DemoPATRONTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column(modifier = Modifier.padding(8.dp)) {
-                        InstructorFormScreen(onSave = { apellido, nombre, horas, pago ->
-                            viewModel.insertInstructor(apellido, nombre, horas, pago)
-                        })
+
+                        // 🟩 Formulario para agregar instructores
+                        InstructorFormScreen(
+                            onSave = { apellido, nombre, horas, pago ->
+                                viewModel.insertInstructor(apellido, nombre, horas, pago)
+                            }
+                        )
+
+                        // 🟦 Lista de instructores
                         InstructorListScreen(viewModel = viewModel)
                     }
                 }
             }
         }
-
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DemoPATRONTheme {
-    }
+    DemoPATRONTheme { }
 }
